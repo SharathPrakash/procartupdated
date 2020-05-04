1dce94c205feeec24a3a0892e0f74bece1bd9489
Sharath <sharath.prakash1992@gmail.com>
2020-05-04 13:58:03 +0800

    Updates

---
+++ precache-manifest.79bcaea61e7fdf4b818ea7e20640306c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9687f6d8ad1b91d36088807c398c1c",
+    "url": "/procartupdated/index.html"
+  },
+  {
+    "revision": "897904a42312fabe603f",
+    "url": "/procartupdated/static/css/main.17166180.chunk.css"
+  },
+  {
+    "revision": "c04efb37817b68d38450",
+    "url": "/procartupdated/static/js/2.9a60879a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/procartupdated/static/js/2.9a60879a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897904a42312fabe603f",
+    "url": "/procartupdated/static/js/main.708a17af.chunk.js"
+  },
+  {
+    "revision": "e44683dc2768d281ba76",
+    "url": "/procartupdated/static/js/runtime-main.09eaaf39.js"
+  }
+]);
